277fcf06fe4009cf8bb303c09f17b4427b0821e4
vamsi <vamsiduvvuru999@gmail.com>
2025-08-09 20:21:12 +0530

    [First commit]

---
+++ src/test/java/com/java/OrderProductService/OrderProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.OrderProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
